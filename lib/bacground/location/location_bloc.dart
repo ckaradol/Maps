b63731a0f517157cb0c543a53e496d056f89729b
ckaradol <70754382+ckaradol@users.noreply.github.com>
2021-09-16 16:27:25 +0300

    Maps

---
+++ lib/bacground/location/location_bloc.dart
@@ -0,0 +1,18 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'location_event.dart';
+part 'location_state.dart';
+
+class LocationBloc extends Bloc<LocationEvent, LocationState> {
+  LocationBloc() : super(LocationInitial());
+
+  @override
+  Stream<LocationState> mapEventToState(
+    LocationEvent event,
+  ) async* {
+ 
+  }
+}
