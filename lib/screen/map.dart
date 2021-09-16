53c8e0edd0de88ce7a53552ec4f15d1069b05a72
ckaradol <70754382+ckaradol@users.noreply.github.com>
2021-09-16 19:02:31 +0300

    maps controller and user location listen

--- lib/screen/map.dart
+++ lib/screen/map.dart
@@ -4,10 +4,17 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:map/bacground/location/location_bloc.dart';
 
-class Maps extends StatelessWidget {
-  const Maps({
+class Maps extends StatefulWidget {
+  Maps({
     Key? key,
   }) : super(key: key);
+
+  @override
+  _MapsState createState() => _MapsState();
+}
+
+class _MapsState extends State<Maps> {
+  MapController controller = MapController();
 
   @override
   Widget build(BuildContext context) {
@@ -15,55 +22,66 @@ class Maps extends StatelessWidget {
       create: (context) => LocationBloc()..add(LocationInitialEvent()),
       child: BlocBuilder<LocationBloc, LocationState>(
         builder: (context, state) {
-          return Scaffold(
-            body: FlutterMap(
-              options: MapOptions(
-                center: LatLng(51.5, -0.09),
-                zoom: 13.0,
-              ),
-              layers: [
-                TileLayerOptions(
-                  urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-                  subdomains: ['a', 'b', 'c'],
+          if (state is LocationSetState) {
+            return Scaffold(
+              body: FlutterMap(
+                mapController: controller,
+                options: MapOptions(
+                  center: state.locationData,
+                  zoom: 5.0,
                 ),
-                MarkerLayerOptions(
-                  markers: [
-                    Marker(
-                      width: 80.0,
-                      height: 80.0,
-                      point: LatLng(51.5, -0.09),
-                      builder: (ctx) => Container(
-                        width: 5,
-                        height: 5,
-                        child: FlutterLogo(),
+                layers: [
+                  TileLayerOptions(
+                    urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                    subdomains: ['a', 'b', 'c'],
+                  ),
+                  MarkerLayerOptions(
+                    markers: [
+                      Marker(
+                        width: 10,
+                        height: 10,
+                        point: LatLng(51.5, -0.09),
+                        builder: (ctx) => Container(
+                          width: 5,
+                          height: 5,
+                          child: FlutterLogo(),
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-                if (state is LocationSetState)
+                    ],
+                  ),
                   MarkerLayerOptions(markers: [
                     Marker(
-                      width: 80.0,
-                      height: 80.0,
-                      point: LatLng(state.locationData.latitude!, state.locationData.longitude!),
+                      width: state.accuracy,
+                      height: state.accuracy,
+                      point: state.locationData,
                       builder: (ctx) => Container(
-                        color: Colors.blue,
+                        width: state.accuracy,
+                        height: state.accuracy,
+                        decoration: BoxDecoration(
+                          color: Colors.blue.shade100,
+                          borderRadius: BorderRadius.circular(100)
+                        ),
+                        child: Icon(Icons.circle,color: Colors.blue,size: 5,)
                       ),
                     ),
                   ]),
-              ],
-            ),
-            floatingActionButton: FloatingActionButton(
-              child: Icon(
-                Icons.location_on_rounded,
-                color: Colors.white,
+                ],
               ),
-              onPressed: () {},
-            ),
-          );
+              floatingActionButton: FloatingActionButton(
+                child: Icon(
+                  Icons.location_on_rounded,
+                  color: Colors.white,
+                ),
+                onPressed: () {
+                  controller.move(state.locationData, controller.zoom);
+                },
+              ),
+            );
+          } else {
+            return Scaffold(body: Center(child: CircularProgressIndicator()));
+          }
         },
       ),
     );
   }
 }
-
