76313f137d6bdee34ccb363c99c91f613a6fda88
ckaradol <70754382+ckaradol@users.noreply.github.com>
2021-09-16 22:04:19 +0300

    animated map move

--- lib/screen/map.dart
+++ lib/screen/map.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 import 'package:map/bacground/data/data_bloc.dart';
 import 'package:map/bacground/location/location_bloc.dart';
 import 'package:map/compenent/userMarker.dart';
@@ -15,24 +16,53 @@ class Maps extends StatefulWidget {
   _MapsState createState() => _MapsState();
 }
 
-class _MapsState extends State<Maps> {
-  MapController controller = MapController();
+class _MapsState extends State<Maps> with TickerProviderStateMixin {
+  final MapController mapController = MapController(); //MapController initialized
+  void _animatedMapMove(LatLng destLocation, double destZoom) {
+    // Create some tweens. These serve to split up the transition from one location to another.
+    // In our case, we want to split the transition be<tween> our current map center and the destination.
+    final _latTween = Tween<double>(begin: mapController.center.latitude, end: destLocation.latitude);
+    final _lngTween = Tween<double>(begin: mapController.center.longitude, end: destLocation.longitude);
+    final _zoomTween = Tween<double>(begin: mapController.zoom, end: destZoom);
+
+    // Create a animation controller that has a duration and a TickerProvider.
+    var controller = AnimationController(duration: const Duration(milliseconds: 500), vsync: this);
+    // The animation determines what path the animation will take. You can try different Curves values, although I found
+
+    Animation<double> animation = CurvedAnimation(parent: controller, curve: Curves.fastOutSlowIn);
+
+    controller.addListener(() {
+      mapController.move(LatLng(_latTween.evaluate(animation), _lngTween.evaluate(animation)), _zoomTween.evaluate(animation));
+    });
+
+    animation.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        controller.dispose();
+      } else if (status == AnimationStatus.dismissed) {
+        controller.dispose();
+      }
+    });
+
+    controller.forward();
+  }
 
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) => LocationBloc()..add(LocationInitialEvent()),
+      create: (context) => LocationBloc()..add(LocationInitialEvent()), //data provider locationBloc starts first
       child: BlocBuilder<LocationBloc, LocationState>(
         builder: (context, state) {
+          // SetStated field when location Bloc state changes
           if (state is LocationSetState) {
             return BlocProvider(
-              create: (context) => DataBloc()..add(DataInitialEvent()),
+              create: (context) => DataBloc()..add(DataInitialEvent()), //data provider dataBloc starts first
               child: BlocBuilder<DataBloc, DataState>(
                 builder: (context, dataState) {
+                  // SetStated field when data Bloc state changes
                   return SafeArea(
                     child: Scaffold(
                       body: FlutterMap(
-                        mapController: controller,
+                        mapController: mapController,
                         options: MapOptions(
                           center: state.locationData,
                           zoom: 5.0,
@@ -59,9 +89,7 @@ class _MapsState extends State<Maps> {
                               ),
                             ),
                           ]),
-
                         ],
-
                         nonRotatedLayers: [
                           ZoomButtonsPluginOption(
                             minZoom: 4,
@@ -78,7 +106,7 @@ class _MapsState extends State<Maps> {
                           color: Colors.white,
                         ),
                         onPressed: () {
-                          controller.move(state.locationData, 17.0);
+                          _animatedMapMove(state.locationData, 17.0);
                         },
                       ),
                     ),
@@ -87,7 +115,11 @@ class _MapsState extends State<Maps> {
               ),
             );
           } else {
-            return Scaffold(body: Center(child: CircularProgressIndicator()));
+            return Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
+              ),
+            ); //loading event
           }
         },
       ),
