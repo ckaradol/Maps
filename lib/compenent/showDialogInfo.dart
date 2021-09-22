e57113fc610da933493e35ac5399c888af5f4653
ckaradol <ckaradol2@gmail.com>
2021-09-22 15:44:08 +0300

    show dialog added

---
+++ lib/compenent/showDialogInfo.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class ShowDialogInfo extends StatelessWidget {
+  const ShowDialogInfo({Key? key, required this.title, this.content})
+      : super(key: key);
+  final String title;
+  final Widget? content;
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text(title),
+      content: content,
+      actions: [
+        TextButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text("OK"),
+        ),
+      ],
+    );
+  }
+}
